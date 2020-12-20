486362c8efc77e0e20beb42a113d2e1970fdfc1c
juliano leonardo soares <julianoleonardosoares@hotmail.com>
2020-12-20 02:25:13 -0300

    first commit

---
+++ src/routes.js
@@ -0,0 +1,25 @@
+const Router = require("koa-router");
+
+// middlewares
+const error = require("./middlewares/error");
+const authenticated = require("./middlewares/auth");
+
+// handlers
+const greetings = require("./handlers/greetings");
+const users = require("./handlers/users");
+const auth = require("./handlers/auth");
+
+const router = new Router();
+
+router.use(error);
+
+router.get("/hello/:name", greetings.hello);
+router.get("/hello", greetings.hello);
+router.get("/", greetings.index);
+
+router.get("/users", users.getAllUsers);
+router.post("/users", authenticated, users.createUser);
+
+router.post("/auth", auth.authenticate);
+
+module.exports = router;
